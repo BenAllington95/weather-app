a5c94dfd2497d23dd9fe0b5f101b3526335360b3
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-04-11 22:24:21 +0100

    Add flex to location section, lining up the location in a row

--- app/src/WeatherSection.jsx
+++ app/src/WeatherSection.jsx
@@ -2,9 +2,14 @@ export default function WeatherSection(props) {
     console.log(props)
     return (
         <div className="weather-data-section">
-            <h1 className="weather-data-location">{props.data.name}</h1>
-            <p>{props.data.country}</p>
-            <img src={props.data.icon} />
+
+            <div className="weather-data-location">
+                <h1 className="weather-data-location-title">{props.data.name}</h1>
+                <p>{props.data.country}</p>
+            </div>
+
+
+            <img className="weather-data-img" src={props.data.icon} />
         </div>
     )
 }
