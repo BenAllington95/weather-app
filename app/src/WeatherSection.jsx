474d4be604698a0bcbc38c5e06d7f11b4fdd0cc4
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-04-12 19:13:53 +0100

    Update WeatherSection.jsx

--- app/src/WeatherSection.jsx
+++ app/src/WeatherSection.jsx
@@ -26,7 +26,7 @@ export default function WeatherSection(props) {
         <div className="weather-data-section">
 
             <div className="weather-data-location">
-                <h1 className="weather-data-location-title">{props.data.name}, </h1>
+                <h1 className="weather-data-location-title">{props.data.name},</h1>
                 <p>{props.data.country}</p>
             </div>
 
