a3ce0567f58813757ebb0404b66d810161dc69ee
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-04-19 21:02:46 +0100

    Add temp and header for weather forecast section

--- app/src/WeatherForecast.jsx
+++ app/src/WeatherForecast.jsx
@@ -3,16 +3,29 @@
 export default function WeatherForecast(props) {
 
     const forecastItems = props.data.map((item, index) => {
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const day = new Date(item.dt_txt).getDay()
+        const dayString = daysOfWeek[day]
         return (
             <div className="weather-forecast-item" key={`item${index}`}>
+                <h3 className="weather-forecast-item-header">{dayString}</h3>
                 <img className="weather-forecast-img" src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@4x.png`} alt="Weather-icon" />
+                <p>{Math.round(item.main.temp)}</p>
             </div>
         )
     })
 
+    
+    console.log(props.data[0].main.temp)
+   
+
+
     return (
         <div className="weather-forecast">
-            {forecastItems}
+            <div className="weather-forecast-items">
+                {forecastItems}
+            </div>
+            
         </div>
     )
 }
