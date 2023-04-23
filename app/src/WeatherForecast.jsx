02c4a20d7fb7335291a8ae17f21779e397852e14
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-04-23 13:16:23 +0100

    Add temperature conversion function, converts forecast temps

--- app/src/WeatherForecast.jsx
+++ app/src/WeatherForecast.jsx
@@ -2,6 +2,20 @@
 
 export default function WeatherForecast(props) {
 
+    function tempConversion(boolean, celsius) {
+        const fahrenheit = (celsius * 9/5) + 32;
+        const symbol = boolean ? '°C' : '°F'
+
+        let value
+        if (boolean) {
+            value = celsius
+        } else {
+            value = fahrenheit
+        }
+
+        return `${Math.round(value)}${symbol}`
+    }
+
     const forecastItems = props.data.map((item, index) => {
         const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         const day = new Date(item.dt_txt).getDay()
@@ -10,7 +24,7 @@ export default function WeatherForecast(props) {
             <div className="weather-forecast-item" key={`item${index}`}>
                 <h3 className="weather-forecast-item-heading">{dayString.slice(0,3)}</h3>
                 <img className="weather-forecast-img" src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@4x.png`} alt="Weather-icon" />
-                <p className="weather-forecast-item-temperature">{Math.round(item.main.temp)}{props.isCelsius ? "°C" : "°F"}</p>
+                <p className="weather-forecast-item-temperature">{tempConversion(props.isCelsius, item.main.temp)}</p>
             </div>
         )
     })
