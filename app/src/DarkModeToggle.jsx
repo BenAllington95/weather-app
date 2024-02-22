d03d1f27eeb0130f80fb9f78aa78fd72bd87ffe4
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2024-02-22 21:12:58 +0000

    ...

--- app/src/DarkModeToggle.jsx
+++ app/src/DarkModeToggle.jsx
@@ -3,12 +3,11 @@ import { useState } from 'react'
 export default function DarkMode(props) {
 
     const style = {
-        background: props.isDarkMode ? "#697384" : "#ECF0F6",
-        color: props.isDarkMode ? "#ECF0F6" : "#697384",
+        background: props.isDarkMode ? "#ECF0F6" : "#697384",
+        color: props.isDarkMode ? "#697384" : "#ECF0F6",
         transition: "all 0.2s ease"
     }
 
-    // background: !isDarkMode ? "#a7d1e9" : "#293b5f",
     return (
         <div onClick={() =>
             props.setIsDarkMode(prevTheme => !prevTheme)} style={style} className="theme-btn">
